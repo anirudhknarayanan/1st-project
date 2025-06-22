f014f9e3d2daff9482943f5e8901b875d89db0ab
anirudh <anirudhknarayanan611@gmail.com>
2025-06-22 15:55:00 +0530

    database model added

---
+++ models/coupenSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const {Schema} = mongoose;
+
+
+const coupenSchema = new Schema({
+    name : {
+        type :String,
+        required : true,
+        unique :true
+    },
+    createdOn : {
+        type :Date,
+        default : Date.now,
+        required: true  
+    },
+    expireOn : {
+         type : Date,
+         required : true
+    },
+    offerPrice : {
+        type : Number,
+        required : true
+    },
+    minimumPrice : {
+        type : Number,
+        required : true 
+
+    },
+    isList:{
+        type : Boolean,
+        default : true
+    },
+    userId : {
+        type : Schema.Types.ObjectId,
+        ref :"User"
+    }
+})
+const Coupen = mongoose.model("Coupen",coupenSchema);
+module.exports = Coupen
