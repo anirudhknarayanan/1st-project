abd53d29b504ad977739c760eca7a95dfa56cfa6
anirudh <anirudhknarayanan611@gmail.com>
2025-08-09 13:01:30 +0530

    productDetails page

--- controllers/user/productController.js
+++ controllers/user/productController.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/productSchema");
 const Category = require("../../models/categorySchema");
-const User = require("../../models/userSchema")
+const User = require("../../models/userSchema");
+const Wishlist = require("../../models/wishlistSchema");
+const Cart = require("../../models/cartSchema")
 
 
 module.exports = {
@@ -10,15 +12,40 @@ module.exports = {
             const userData = await User.findById(userId).lean()
             const productId = req.query.id;
             console.log("its my productId", productId)
-            const product = await Product.findById(productId).populate('category').lean()
+            const product = await Product.findById(productId).populate('category').lean();
             console.log(product);
 
-             if (!product || product.isBlocked) {
-      return res.status(404).render("user/page-404", {
-        user: userData,
-        message: "Product not found or is blocked",
-      });
-    }
+
+            if (!product || product.isBlocked) {
+                return res.status(404).render("user/page-404", {
+                    user: userData,
+                    message: "Product not found or is blocked",
+                });
+            }
+
+            let isinWishlist, isinCart = false;
+
+            if (userId) {
+                const wishlist = await Wishlist.findOne({
+                    userId: userId,
+                    "items.productId": productId
+
+                })
+                if (wishlist) {
+                    isinWishlist = true;
+                }
+            }
+
+
+            if (userId) {
+                const cart = await Cart.findOne({
+                    userId: userId,
+                    "items.productId": productId
+                });
+                if (cart) {
+                    isinCart = true;
+                }
+            }
 
             const findCategory = product.category;
             const categoryOffer = findCategory?.categoryOffer || 0;
@@ -31,6 +58,8 @@ module.exports = {
                 quantity: product.quantity,
                 totalOffer: totalOffer,
                 category: findCategory,
+                isinWishlist,
+                isinCart
 
             })
 
